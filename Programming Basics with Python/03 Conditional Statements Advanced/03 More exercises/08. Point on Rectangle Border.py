74f0f0d29a43b9cb03ce4747bb54f4d40bf79b2d
ivaylospasov <3828866+ivaylospasov@users.noreply.github.com>
2025-03-05 07:41:40 +0200

    Conditional Statements Advanced - More exercises - ready

--- Programming Basics with Python/03 Conditional Statements Advanced/03 More exercises/08. Point on Rectangle Border.py
+++ Programming Basics with Python/03 Conditional Statements Advanced/03 More exercises/08. Point on Rectangle Border.py
@@ -1,11 +1,16 @@
-x1 = int(input())
-x2 = int(input())
-y1 = int(input())
-y2 = int(input())
-x = int(input())
-y = int(input())
+x1 = float(input())
+y1 = float(input())
+x2 = float(input())
+y2 = float(input())
+x = float(input())
+y = float(input())
 
-if (x == x1 and y1 <= y <= y2) or (x == x2 and y1 <= y <= y2) or (y == y1 and x1 <= x <= x2) or (y == y2 and x1 <= x <= x2):
-    print('Border')
+if ((x == x1 and y1 <= y <= y2) or
+        (x == x2 and y1 <= y <= y2) or
+        (y == y1 and x1 <= x <= x2) or
+        (y == y2 and x1 <= x <= x2)):
+    output = 'Border'
+else:
+    output = 'Inside / Outside'
 
-# TODO find outside and inside options
+print(output)
---
+++ Programming Basics with Python/03 Conditional Statements Advanced/03 More exercises/10. Multiply by 2.py
@@ -0,0 +1,8 @@
+my_number = float(input())
+
+while my_number >= 0:
+    result = my_number * 2
+    print(f'Result: {result:.2f}')
+    my_number = float(input())
+
+print("Negative number!")
