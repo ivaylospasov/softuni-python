fa75b6cdec6c6b48cdd43e2359cbf89bd77706c6
ivaylospasov <3828866+ivaylospasov@users.noreply.github.com>
2025-03-10 21:25:23 +0200

    dishwasher

--- Programming Basics with Python/05 While Loop/03 More exercises/01. Dishwasher.py
+++ Programming Basics with Python/05 While Loop/03 More exercises/01. Dishwasher.py
@@ -1,6 +1,6 @@
-dishwasher_bottles = int(input()) # bottles
+dishwasher_bottles = int(input()) # number of dishwasher bottles
 
-bottle_volume = 750
+bottle_volume = 750 # The volume of one dishwasher bottle in milliliters
 
 dirty_dishes = 0
 dirty_pots = 0
