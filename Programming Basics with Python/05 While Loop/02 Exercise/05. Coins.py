39e15329697a7fbf3bffeeeeea77a5dfaaa5bca6
ivaylospasov <3828866+ivaylospasov@users.noreply.github.com>
2025-03-10 09:27:48 +0200

    While loop - More exercise - до задача 5 включително

--- Programming Basics with Python/05 While Loop/02 Exercise/05. Coins.py
+++ Programming Basics with Python/05 While Loop/02 Exercise/05. Coins.py
@@ -1,58 +1,62 @@
-change = float(input())
+change_in_leva = float(input())
 
-leva_2 = 0
-leva_1 = 0
-coins_50 = 0
-coins_20 = 0
-coins_10 = 0
-coins_05 = 0
-coins_02 = 0
-coins_01 = 0
+leva_2: int = int(0)
+leva_1: int = int(0)
+stotinki_50: int = int(0)
+stotinki_20: int = int(0)
+stotinki_10: int = int(0)
+stotinki_5: int = int(0)
+stotinki_2: int = int(0)
+stotinki_1: int = int(0)
 
-change_left = 0
-coins_counter: int = 0
+coins_counter: int = int(0)
 
-change_in_coins: float = change * 100
+change_in_stotinki: int = int(change_in_leva * 100)
 
+if change_in_leva < 0:
+    raise ValueError('The change cannot be negative')
 
-if change_in_coins >= 200:
-    leva_2 = change_in_coins // 200
+if change_in_leva == 0:
+    pass
+
+if change_in_stotinki >= 200:
+    leva_2 = int(change_in_stotinki // 200)
     coins_counter += leva_2
-    change_in_coins = change_in_coins - (leva_2 * 200)
+    change_in_stotinki = change_in_stotinki - (leva_2 * 200)
 
-if 100 <= change_in_coins < 200:
-    leva_1 = change_in_coins // 100
+if 100 <= change_in_stotinki < 200:
+    leva_1 = int(change_in_stotinki // 100)
     coins_counter += leva_1
-    change_in_coins = change_in_coins - (leva_1 * 100)
+    change_in_stotinki = change_in_stotinki - (leva_1 * 100)
 
-if 50 <= change_in_coins < 100:
-    coins_50 = 1
-    coins_counter += coins_50
-    change_in_coins = change_in_coins - (coins_50 * 50)
+if 50 <= change_in_stotinki < 100:
+    stotinki_50 = int(1)
+    coins_counter += stotinki_50
+    change_in_stotinki = change_in_stotinki - (stotinki_50 * 50)
 
-if 20 <= change_in_coins < 50:
-    coins_20 = change_in_coins // 20
-    coins_counter += coins_20
-    change_in_coins = change_in_coins - (coins_20 * 20)
+if 20 <= change_in_stotinki < 50:
+    stotinki_20 = int(change_in_stotinki // 20)
+    coins_counter += stotinki_20
+    change_in_stotinki = change_in_stotinki - (stotinki_20 * 20)
 
-if 10 <= change_in_coins < 20:
-    coins_10 = change_in_coins // 10
-    coins_counter += coins_10
-    change_in_coins = change_in_coins - (coins_10 * 10)
+if 10 <= change_in_stotinki < 20:
+    stotinki_10 = int(change_in_stotinki // 10)
+    coins_counter += stotinki_10
+    change_in_stotinki = change_in_stotinki - (stotinki_10 * 10)
 
-if 5 <= change_in_coins < 10:
-    coins_05 = change_in_coins // 5
-    coins_counter += coins_05
-    change_in_coins = change_in_coins - (coins_05 * 5)
+if 5 <= change_in_stotinki < 10:
+    stotinki_5 = int(change_in_stotinki // 5)
+    coins_counter += stotinki_5
+    change_in_stotinki = change_in_stotinki - (stotinki_5 * 5)
 
-if 2 < change_in_coins < 5:
-    coins_02 = change_in_coins // 2
-    coins_counter += coins_02
-    change_in_coins = change_in_coins - (coins_02 * 2)
+if 2 <= change_in_stotinki < 5:
+    stotinki_2 = int(change_in_stotinki // 2)
+    coins_counter += stotinki_2
+    change_in_stotinki = change_in_stotinki - (stotinki_2 * 2)
 
-if 1 <= change_in_coins < 2:
-    coins_01 = 1
+if 1 <= change_in_stotinki < 2:
+    stotinki_1 = int(1)
     coins_counter += 1
-    change_in_coins = change_in_coins - coins_01
+    change_in_stotinki = change_in_stotinki - stotinki_1
 
 print(int(coins_counter))
