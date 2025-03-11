2e161ed9a3d3f05dcf94f7762f41ceba594290b7
ivaylospasov <3828866+ivaylospasov@users.noreply.github.com>
2025-03-11 09:09:43 +0200

    Report system ready

--- Programming Basics with Python/05 While Loop/03 More exercises/02. Report System.py
+++ Programming Basics with Python/05 While Loop/03 More exercises/02. Report System.py
@@ -4,20 +4,24 @@ current_charity_sum = 0
 current_charity_sum_cash = 0
 current_charity_sum_card = 0
 
-transaction_count = 0
+transaction_tries = 0
 transaction_count_cash = 0
 transaction_count_card = 0
 
+average_cash_charity = 0
+average_card_charity = 0
+
 while current_charity_sum < charity_sum_expected:
     current_sum = input()
+
     if current_sum == "End":
-        print("Failed to raise required money for charity.")
+        print("Failed to collect required money for charity.")
         break
 
-    current_sum = float(current_sum)
-    transaction_count += 1
+    current_sum = int(current_sum)
+    transaction_tries += 1
 
-    if transaction_count % 2 != 0:
+    if transaction_tries % 2 != 0:
         if current_sum > 100:
             print("Error in transaction!")
         else:
@@ -34,6 +38,13 @@ while current_charity_sum < charity_sum_expected:
             current_charity_sum += current_sum
             transaction_count_card += 1
 
+
 if current_charity_sum >= charity_sum_expected:
-    print(f"Average CS: {current_charity_sum_cash / transaction_count_cash}")
-    print(f"Average CC: {current_charity_sum_card / transaction_count_card}")
+    if transaction_count_cash > 0:
+        average_cash_charity = current_charity_sum_cash / transaction_count_cash
+
+    if transaction_count_card > 0:
+        average_card_charity = current_charity_sum_card / transaction_count_card
+
+    print(f"Average CS: {average_cash_charity:.2f}")
+    print(f"Average CC: {average_card_charity:.2f}")
