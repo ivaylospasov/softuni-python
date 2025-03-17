722a540e742717be86d7e4fdd989cc71d0d1ce80
ivaylospasov <3828866+ivaylospasov@users.noreply.github.com>
2025-03-17 17:28:46 +0200

    update code

--- Programming Basics with Python/06 Nested Loops/02 Exercise/02_equal_sums_even_odd_position.py
+++ Programming Basics with Python/06 Nested Loops/02 Exercise/02_equal_sums_even_odd_position.py
@@ -1,3 +1,5 @@
+from icecream import ic
+
 number_one = int(input())
 number_two = int(input())
 sum_odd_numbers = 0
@@ -5,10 +7,12 @@ sum_even_numbers = 0
 
 for number in range(number_one, number_two):
     number_string = str(number)
-    for index in range(len(number_string)):
-        if int(number_string[index]) % 2 != 0:
+    for index in range(0, len(number_string)):
+        if index % 2 != 0:
             sum_odd_numbers += int(number_string[index])
+            ic(sum_odd_numbers)
         else:
             sum_even_numbers += int(number_string[index])
+            ic(sum_even_numbers)
     if sum_odd_numbers == sum_even_numbers:
         print(number_string)
