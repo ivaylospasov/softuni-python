90c06f4a156e9dedf6cc52c6c75442e14edc0e92
ivaylospasov <3828866+ivaylospasov@users.noreply.github.com>
2025-03-26 16:03:03 +0200

    задача 2 60 %

--- Programming Basics with Python/06 Nested Loops/02 Exercise/02_equal_sums_even_odd_position.py
+++ Programming Basics with Python/06 Nested Loops/02 Exercise/02_equal_sums_even_odd_position.py
@@ -1,24 +1,32 @@
-from icecream import ic
+#TODO Защо на някои тестове се получава грешен отговор? Защо в упътването искат в range второто число да е +1?
 
+# Четене на входните числа
 number_one = int(input())
 number_two = int(input())
-sum_odd_numbers = 0
-sum_even_numbers = 0
-list_results = []
 
+# Списък за всички числа, които отговарят на условието
+result_numbers = []
+
+# Итерация през числата между number_one и number_two
 for number in range(number_one + 1, number_two):
-    number_string = str(number)
-    for index in range(len(number_string)):
-        if (index+1) % 2 != 0:
-            sum_odd_numbers += int(number_string[index])
-            # ic(sum_odd_numbers)
-        else:
-            sum_even_numbers += int(number_string[index])
-            # ic(sum_even_numbers)
-    if sum_odd_numbers == sum_even_numbers:
-        # print(number_string + ' ', end=' ')
-        list_results.append(number_string)
-        sum_odd_numbers = 0
-        sum_even_numbers = 0
+    sum_odd_positions = 0
+    sum_even_positions = 0
 
-ic(list_results)
+    # Преобразуване на числото в стринг за достъп до отделните цифри
+    number_string = str(number)
+
+    # Изчисляване на сумите на четните и нечетните позиции
+    for index in range(len(number_string)):
+        digit = int(number_string[index])
+        if (index + 1) % 2 != 0:  # Нечетна позиция
+            sum_odd_positions += digit
+        else:  # Четна позиция
+            sum_even_positions += digit
+
+    # Проверка дали сумите са равни
+    if sum_odd_positions == sum_even_positions:
+        result_numbers.append(number)
+
+# Принтиране на резултатите
+if result_numbers:
+    print(" ".join(map(str, result_numbers)))
