dd7b3808a117b39bf55086de5d1deeb64044fc08
ivaylospasov <3828866+ivaylospasov@users.noreply.github.com>
2025-05-13 15:38:47 +0300

    Add initial implementation for Easter bread budget calculation
    This script calculates the cost of making Easter bread based on user input for budget and the price of flour. It also computes the prices for eggs and milk proportionally to the flour price.

---
+++ 02 PYTHON FUNDAMENTALS/01 Basic Syntax, Conditional Statements and Loops/02 Exercise/11_easter_bread.py
@@ -0,0 +1,6 @@
+budget = float(input())
+flower_price_per_kg = float(input())
+
+pack_of_eggs_price = 0.75 * flower_price_per_kg
+milk_per_litre_price = 1.25 * flower_price_per_kg # use only 250 ml per bread
+
