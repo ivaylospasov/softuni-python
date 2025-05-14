0dc238e0f4bd745c23db8edf39f586a561a53658
ivaylospasov <3828866+ivaylospasov@users.noreply.github.com>
2025-05-14 20:48:26 +0300

    started the last problem of basic

--- 02 PYTHON FUNDAMENTALS/01 Basic Syntax, Conditional Statements and Loops/02 Exercise/11_easter_bread.py
+++ 02 PYTHON FUNDAMENTALS/01 Basic Syntax, Conditional Statements and Loops/02 Exercise/11_easter_bread.py
@@ -4,3 +4,16 @@ flower_price_per_kg = float(input())
 pack_of_eggs_price = 0.75 * flower_price_per_kg
 milk_per_litre_price = 1.25 * flower_price_per_kg # use only 250 ml per bread
 
+bread_count = 0
+colored_eggs_count = 0
+
+one_bread_costs = flower_price_per_kg + pack_of_eggs_price + milk_per_litre_price / 4
+
+while budget >= one_bread_costs:
+    budget -= one_bread_costs
+    bread_count += 1
+    colored_eggs_count += 3
+    if bread_count % 3 == 0:
+        colored_eggs_count -= bread_count - 2
+
+print(f'You made {bread_count} loaves of Easter bread! Now you have {colored_eggs_count} eggs and {budget:.2f}BGN left.')
---
+++ 02 PYTHON FUNDAMENTALS/01 Basic Syntax, Conditional Statements and Loops/02 Exercise/12_christmas_spirit.py
@@ -0,0 +1,17 @@
+quantity_of_decorations = int(input())
+days_left_until_christmas = int(input())
+
+total_cost = 0
+
+ornament_set_price = 2
+ornament_set_points = 5
+
+tree_skirt_price = 5
+tree_skirt_points = 3
+
+tree_garland_price = 3
+tree_garland_points = 10
+
+tree_lights_price = 15
+tree_lights_points = 17
+
