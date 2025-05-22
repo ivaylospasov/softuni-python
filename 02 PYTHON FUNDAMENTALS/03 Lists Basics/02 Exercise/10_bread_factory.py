ce7043e52fd971414b1ad8e6f8f0148b11a5d0fa
ivaylospasov <3828866+ivaylospasov@users.noreply.github.com>
2025-05-22 06:11:37 +0300

    Problem 10 partially solved

--- 02 PYTHON FUNDAMENTALS/03 Lists Basics/02 Exercise/10_bread_factory.py
+++ 02 PYTHON FUNDAMENTALS/03 Lists Basics/02 Exercise/10_bread_factory.py
@@ -1,5 +1,5 @@
-initial_energy = 100
-initial_coins = 100
+energy = 100
+coins = 100
 
 events = input().split("|")
 events_handled = 0
@@ -12,30 +12,30 @@ for element in events:
 for event in events_elements_as_list:
     if event[0] == "rest":
         energy_change = int(event[1])
-        initial_energy += energy_change
-        if initial_energy > 100:
-            initial_energy = 100
+        energy += energy_change
+        if energy > 100:
+            energy = 100
             energy_change = 0
-            print(f'You gained {energy_change} energy.')
-        else:
-            print(f'You gained {energy_change} energy.')
-        print(f"Current energy: {initial_energy}.")
+        print(f'You gained {energy_change} energy.')
+        print(f"Current energy: {energy}.")
         events_handled += 1
     elif event[0] == "order":
-        if initial_energy >= 30:
-            initial_energy -= 30
+        if energy >= 30:
+            energy -= 30
             coins_change = int(event[1])
-            initial_coins += coins_change
-            print(f'You earned {coins_change} coins.')
+            coins += coins_change
             events_handled += 1
+            print(f'You earned {coins_change} coins.')
         else:
-            initial_energy += 50
+            energy += 50
+            if energy > 100:
+                energy = 100
             print(f'You had to rest!')
     else:
         ingredient = event[0]
         ingredient_price = int(event[1])
-        if initial_coins >= ingredient_price:
-            initial_coins -= ingredient_price
+        if coins >= ingredient_price:
+            coins -= ingredient_price
             print(f'You bought {ingredient}.')
             events_handled += 1
         else:
@@ -45,5 +45,7 @@ for event in events_elements_as_list:
 
 if events_handled == len(events_elements_as_list):
     print(f"Day completed!")
-    print(f"Coins: {initial_coins}")
-    print(f"Energy: {initial_energy}")
+    print(f"Coins: {coins}")
+    print(f"Energy: {energy}")
+
+# TODO Correct the code!
